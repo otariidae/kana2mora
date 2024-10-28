00c53366e1d6f9f360573acc78ba7725b321f471
otariidae <otariidae@users.noreply.github.com>
2024-10-28 20:50:46 +0900

    0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.3.0
+
+- update Deno version to v2 for development
+
 ## 0.2.0
 
 - introduce dual CommonJS/ECMAScript module package
--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -11,7 +11,7 @@ await build({
   package: {
     // package.json properties
     name: "kana2mora",
-    version: "0.2.0",
+    version: "0.3.0",
     description: "Divide a string of Japanese Kana into mora",
     repository: {
       "type": "git",
